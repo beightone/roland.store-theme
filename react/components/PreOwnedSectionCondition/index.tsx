8381a2232842accff612b0191f638cbd911bd531
Gabriel Gotardo <87941903+gabgotardo@users.noreply.github.com>
2024-09-23 12:27:46 -0300

    refactor: Clean up unused code and improve styling in pre-owned sections

--- react/components/PreOwnedSectionCondition/index.tsx
+++ react/components/PreOwnedSectionCondition/index.tsx
@@ -12,7 +12,13 @@ const PreOwnedSectionCondition = ({ children }: { children: ReactNode }) => {
 
       const data = await response.json()
 
-      console.log('data.length', data.length > 0)
+      // const isAvailable = data?.map((product: any) =>
+      //   product.items.some(
+      //     (item: any) => item.sellers[0].commertialOffer.AvailableQuantity > 0
+      //   )
+      // )
+
+      // console.log('data.length', { data, isAvailable })
       setHasPreOwnedProducts(data?.length > 0)
     } catch (error) {
       console.error('Error while fetching pre-owned products', error)
