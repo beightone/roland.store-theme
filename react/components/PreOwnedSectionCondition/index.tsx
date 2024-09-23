97739da8bf704c027c36247894ffd4dff1d2ede1
Gabriel Gotardo <87941903+gabgotardo@users.noreply.github.com>
2024-09-23 13:20:52 -0300

    refactor: Update availability check for pre-owned products in PreOwnedSectionCondition

--- react/components/PreOwnedSectionCondition/index.tsx
+++ react/components/PreOwnedSectionCondition/index.tsx
@@ -12,14 +12,14 @@ const PreOwnedSectionCondition = ({ children }: { children: ReactNode }) => {
 
       const data = await response.json()
 
-      // const isAvailable = data?.map((product: any) =>
-      //   product.items.some(
-      //     (item: any) => item.sellers[0].commertialOffer.AvailableQuantity > 0
-      //   )
-      // )
+      const isAvailable = data?.map((product: any) =>
+        product.items.some(
+          (item: any) => item.sellers[0].commertialOffer.AvailableQuantity > 0
+        )
+      )
 
-      // console.log('data.length', { data, isAvailable })
-      setHasPreOwnedProducts(data?.length > 0)
+      console.log('data.length', { data, isAvailable })
+      setHasPreOwnedProducts(isAvailable && data?.length > 0)
     } catch (error) {
       console.error('Error while fetching pre-owned products', error)
     }
