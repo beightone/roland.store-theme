320eaa24a1a140e163fb84968ccd34c1319dba52
Gabriel Gotardo <87941903+gabgotardo@users.noreply.github.com>
2024-10-14 11:39:33 -0300

    feat: Add CustomFlagDiscount component and integrate styles for Black Friday

---
+++ react/components/CustomFlagDiscount/index.tsx
@@ -0,0 +1,78 @@
+import React, { Fragment } from 'react'
+import { useProduct } from 'vtex.product-context'
+
+// Types
+interface CustomFlagDiscountProps {
+  children: React.ReactNode
+  flagType: string
+  backgroundColor: string
+  color: string
+}
+
+const CustomFlagDiscount: StorefrontFunctionComponent<
+  CustomFlagDiscountProps
+> = ({ children, flagType, backgroundColor, color }) => {
+  const { product } = useProduct() ?? {}
+
+  const hasDiscount =
+    product?.priceRange?.listPrice.highPrice !==
+    product?.priceRange?.sellingPrice.highPrice
+
+  if (!hasDiscount) {
+    return null
+  }
+
+  const style: React.CSSProperties = {
+    backgroundColor,
+    color,
+    fontFamily: 'Monument Extended',
+    fontSize: flagType === '-n%' ? '14px' : '9px',
+    borderRadius: flagType === '-n%' ? '16px' : '50%',
+    padding: flagType === '-n%' ? '4px' : '9px 4px',
+    display: 'flex',
+    flexDirection: flagType === '-n%' ? 'row' : 'column',
+  }
+
+  return (
+    <div style={style}>
+      <Fragment>
+        {flagType === '-n%' && <span>-</span>}
+        {children}
+        {flagType === 'n%off' && (
+          <span style={{ fontSize: '12px', fontWeight: '700' }}>OFF</span>
+        )}
+      </Fragment>
+    </div>
+  )
+}
+
+CustomFlagDiscount.schema = {
+  title: 'Tipo de flag',
+  type: 'object',
+  properties: {
+    flagType: {
+      title: 'Qual o tipo de flag?',
+      enum: ['-n%', 'n%off'],
+      enumNames: ['-50%', '50% OFF'],
+      default: '-n%',
+    },
+    backgroundColor: {
+      title: 'Cor de fundo',
+      type: 'string',
+      widget: {
+        'ui:widget': 'color',
+      },
+      default: '#FF5A00',
+    },
+    color: {
+      title: 'Cor do texto',
+      type: 'string',
+      widget: {
+        'ui:widget': 'color',
+      },
+      default: '#FFFFFF',
+    },
+  },
+}
+
+export default CustomFlagDiscount
