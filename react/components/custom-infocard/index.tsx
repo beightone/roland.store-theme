a3de2fd80e7e16a9a188d0e9a00dfee5594951b7
Beatriz Cavalieri <beatriz@TEC018044>
2024-10-24 11:58:46 -0300

    feat: add condition layout to discount banners and categories structure cards adjustments

--- react/components/custom-infocard/index.tsx
+++ react/components/custom-infocard/index.tsx
@@ -1,12 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Dependencies
-import React, { Fragment } from 'react'
+import React from 'react'
 
 // Styles
 import styles from './styles.css'
-
-// Components
-import { SliderLayout } from 'vtex.slider-layout'
 
 // Context
 import { CUSTOM_INFOCARD_SCHEMA } from './schema'
@@ -14,77 +10,51 @@ import { CUSTOM_INFOCARD_SCHEMA } from './schema'
 // Hooks
 // @ts-ignore
 import { useDevice } from 'vtex.device-detector'
-
 // Interface
 interface CustomInfoCardProps {
   cards: any
+  cardSeeAll: any
 }
 
-const configSlider: any = {
-  itemsPerPage: {
-    desktop: 4,
-    tablet: 3,
-    phone: 2,
-  },
-  infinite: true,
-  usePagination: true,
-  showNavigationArrows: 'desktopOnly',
-  showPaginationDots: 'always',
-}
-
-const CustomInfoCard = ({ cards }: CustomInfoCardProps) => {
+const CustomInfoCard = ({ cards, cardSeeAll }: CustomInfoCardProps) => {
   const { isMobile } = useDevice()
 
-  if (!cards) {
+  if (!cards || !cardSeeAll) {
     return null
   }
 
+  const filteredCards = cards.slice(0, isMobile ? 5 : 7)
+
+  console.log('cards', isMobile, filteredCards)
+
   return (
     <div className={styles['cards-container']}>
-      {!isMobile ? (
-        <SliderLayout {...configSlider}>
-          {cards.map((card: any) => {
-            const { image, hoverImage, title, link, buttonLabel } = card
+      {filteredCards.map((card: any) => {
+        const { image, hoverImage, title, link, buttonLabel } = card
 
-            return (
-              <a href={link} key={image}>
-                <div className={styles['card-wrapper']}>
-                  <div className={styles['card-image']}>
-                    <img src={image} alt={title} />
-                  </div>
-                  <div className={styles['hover-image']}>
-                    <img src={hoverImage} alt={title} />
-                  </div>
-                  <div className={styles['card-content']}>
-                    <h3>{title}</h3>
-                    <button>{buttonLabel}</button>
-                  </div>
-                </div>
-              </a>
-            )
-          })}
-        </SliderLayout>
-      ) : (
-        <Fragment>
-          {cards.map((card: any) => {
-            const { image, title, link, buttonLabel } = card
-
-            return (
-              <a href={link} key={image}>
-                <div className={styles['card-wrapper']}>
-                  <div className={styles['card-image']}>
-                    <img src={image} alt={title} />
-                  </div>
-                  <div className={styles['card-content']}>
-                    <h3>{title}</h3>
-                    <button>{buttonLabel}</button>
-                  </div>
-                </div>
-              </a>
-            )
-          })}
-        </Fragment>
-      )}
+        return (
+          <a className={styles['card-link']} href={link} key={image}>
+            <div className={styles['card-wrapper']}>
+              <div className={styles['card-image']}>
+                <img src={image} alt={title} />
+              </div>
+              <div className={styles['hover-image']}>
+                <img src={hoverImage} alt={title} />
+              </div>
+              <div className={styles['card-content']}>
+                <h3>{title}</h3>
+                <button>{buttonLabel}</button>
+              </div>
+            </div>
+          </a>
+        )
+      })}
+      <div className={styles['see-all-card']}>
+        <span className={styles['see-all-card-title']}>{cardSeeAll.title}</span>
+        <a className={styles['see-all-card-button']} href={cardSeeAll.link}>
+          {cardSeeAll.buttonLabel}
+        </a>
+      </div>
     </div>
   )
 }
